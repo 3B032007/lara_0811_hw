c65736806507c93a3a761b99dda8d883fe296208
劉秉樺 <alen920617@gmail.com>
2023-10-13 17:00:53 +0800

    新增homecontroller、aboutcontroller、newscontroller並分別回傳home.blade.php、about.blade.php、及news.blade.php三個view。

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home',[HomeController::class,'index']) -> name('home.index');
+Route::get('home',[AboutController::class,'index']) -> name('about.index');
+Route::get('home',[NewsController::class,'index']) -> name('news.index');
